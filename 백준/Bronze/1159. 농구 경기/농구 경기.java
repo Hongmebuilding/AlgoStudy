42d258dd9310869b3af41fe8bdab560704f45cbe
LeeSumin <87316411+Hongmebuilding@users.noreply.github.com>
2023-11-23 22:10:46 +0900

    [Bronze II] Title: 농구 경기, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int N = Integer.parseInt(br.readLine());
+        String name = "";
+
+        int[] arr = new int[26];
+        List<Character> list = new ArrayList<>();
+
+        for(int i = 0; i< N; i++){
+            name = br.readLine();
+
+            arr[name.charAt(0)-97]++;
+        }
+
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] >=5) {
+                list.add((char)(i+97));
+            }
+        }
+
+        if(list.size() == 0){
+            pw.println("PREDAJA");
+        } else {
+            for(int i = 0; i<list.size(); i++){
+                pw.print(list.get(i));
+            }
+        }
+        pw.close();
+        br.close();
+    }
+}
