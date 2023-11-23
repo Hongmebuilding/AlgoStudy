df28b6432105fdea0ba6c3705410561a95b2e62a
LeeSumin <87316411+Hongmebuilding@users.noreply.github.com>
2023-11-23 22:11:38 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,52 +1,40 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.PrintWriter;
 
 public class Main {
-	
-	static int N;
-	static String[] arr;
-	static int cnt = 0;
-	
-	static boolean isGroup(String str) {
-		
-		Map<Character, Integer> map = new HashMap<>();
-		
-		//단어 순회
-		for (int i = 0; i < str.length(); i++) {
-			char now = str.charAt(i);
-			//현재 글자가 없다면 추가 map에 추가 ㄱㄱ
-			if(!map.containsKey(now)) { // 같은 글자 아니면
-				map.put(now, i); // b : 0,a : 1,n : 2 banana의 새로운 글자만 할 의도
-			}else {//같은 글자면
-				//연속되었을까?
-				if(map.get(now) != i-1) { // 현재 글자의 인덱스와 이전에 같은 글자가 나왔던 마지막 인덱스가 연속적이지 않을 때
-					return false;
-				}	
-				map.put(now, i);//같은글자이고 현재 글자의 인덱스와 그전에 나왔던 인덱스가 연속적이면 map에 넣어주라
-			}
-		}
-		return true;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new String[N];
-		
-		for (int i = 0; i < N; i++) {
-			arr[i] = br.readLine();
-		}
-		
-		for (int i = 0; i < N; i++) {
-			if(isGroup(arr[i])) {
-				cnt++;
-			}
-		}
-		
-		System.out.println(cnt);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        int N = Integer.parseInt(br.readLine());
+        int total = 0;
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            boolean isGroup = true;
+
+            for (int j = 0; j < s.length()-1; j++) {
+                char currentChar = s.charAt(j);
+                char nextChar = s.charAt(j+1);
+                if(currentChar != nextChar && s.substring(j+1).contains(String.valueOf(currentChar))){
+                    isGroup=false;
+                    break;
+                }
+            }
+
+            if(isGroup){
+                total++;
+            }
+
+
+        }
+
+        pw.println(total);
+
+        pw.close();
+        br.close();
+    }
 }
