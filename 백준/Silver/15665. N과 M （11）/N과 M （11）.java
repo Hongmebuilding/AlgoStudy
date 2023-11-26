2abdf9369bddabb0dac6d3d7e153e9c3cf9f5197
LeeSumin <87316411+Hongmebuilding@users.noreply.github.com>
2023-11-26 16:13:25 +0900

    [Silver II] Title: N과 M (11), Time: 1392 ms, Memory: 326468 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int arr[], ans[], N, M;
+
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        ans = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        nm(arr, 0);
+
+        for (String sb : set) {
+            pw.println(sb);
+        }
+
+        br.close();
+        pw.close();
+    }
+
+    public static void nm(int[] arr, int cnt) {
+        if (cnt == M) {
+            String s = "";
+            for (int i : ans) {
+                s += i;
+                s += " ";
+            }
+            set.add(s);
+            cnt = 0;
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            ans[cnt] = arr[i];
+            nm(arr, cnt + 1);
+        }
+    }
+}
