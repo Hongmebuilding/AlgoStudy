2ac8fbddaea2b917d0d1fb87c093b5f587ac8b52
LeeSumin <87316411+Hongmebuilding@users.noreply.github.com>
2024-03-14 23:41:10 +0900

    [Silver III] Title: 1, Time: 208 ms, Memory: 17212 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int num = 0;
+            for (int i = 1; ; i++) {
+                num = num * 10 + 1;
+                num %= n;
+                if (num == 0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
+
